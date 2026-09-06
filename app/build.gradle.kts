c6dee278f8d4309f92c7300667cc5e19210141be
plbogen2 <plbogen@google.com>
2026-09-05 21:42:33 -0700

    chore: adjust version to 1.1.0 for Firebase RTDB backend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = buildNumber
-        versionName = "2.0.0.$buildNumber"
+        versionName = "1.1.0.$buildNumber"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
